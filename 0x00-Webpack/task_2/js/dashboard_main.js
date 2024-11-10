95dfb31c3a0168045a8576bd7ec046381e3afe76
KalminX <noriabure@gmail.com>
2024-11-10 02:23:53 +0100

    feat: solved the third task

---
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,18 @@
+import $ from "jquery";
+var _ = require("lodash");
+
+$(document).ready(function () {
+  $("body").append("<p>Holberton Dashboard</p>");
+  $("body").append("<p>Dashboard data for the students</p>");
+  $("body").append("<button>Click here to get started</button>");
+  $("body").append('<p id="count"></p>');
+  $("body").append("<p>Copyright - Holberton School</p>");
+  let i = 0;
+
+  function updateCounter() {
+    i += 1;
+    $("#count").text(i);
+  }
+
+  $("button").on("click", _.debounce(updateCounter, 500));
+});
